6e82511ed089d4cade92af4176f406da46861ed3
Martin Lenart <martin_lenart@icloud.com>
2022-12-30 14:23:45 +0100

    update

--- ThreadSafeData/Program.cs
+++ ThreadSafeData/Program.cs
@@ -7,10 +7,24 @@ namespace ThreadSafeData
     class Program
     {
         //Thread Safe Datastructure
-        public class Vehicle
+        public class ApplicationConfig
         {
+            static ApplicationConfig _instance = null;
+            public static ApplicationConfig Instance { 
+                get 
+                {
+                    object _locker = new object();
+                    lock (_locker)
+                    {
+                        if (_instance == null)
+                            _instance = new ApplicationConfig();
+                        return _instance;
+                    }
+                } 
+            }
+
             object _locker = new object();
-            string RegistrationNumber;
+            string RegistrationNumber; 
             string Owner;
 
             public void SetData(string regNr, string owner)
@@ -25,25 +39,42 @@ namespace ThreadSafeData
             {
                 lock (_locker) { return (RegistrationNumber, Owner); }
             }
+
+            private ApplicationConfig()
+            {
+                Owner = "Initial configuration";
+                RegistrationNumber = "Initial regnummer";
+            }
         }
         static void Main(string[] args)
         {
-            var myCar = new Vehicle();
             var rnd = new Random();
 
+            var appconfig1 = ApplicationConfig.Instance;
+            Console.WriteLine(appconfig1.GetData());
+            appconfig1.SetData("Hello", "Hello");
+
+            var appconfig2 = ApplicationConfig.Instance;
+            Console.WriteLine(appconfig2.GetData());
+
+            var appconfig3 = ApplicationConfig.Instance;
+            Console.WriteLine(appconfig3.GetData());
+
+            
             var t1 = Task.Run(() =>
             {
+                var localAppConfig = ApplicationConfig.Instance;
                 var rnd = new Random();
                 for (int i = 0; i < 1000; i++)
                 {
                     //Write Data to Vehicle, "ABC 123", "Kalle Anka"
-                    myCar.SetData("ABC 123", "Kalle Anka");
+                    localAppConfig.SetData("ABC 123", "Kalle Anka");
 
                     //introduce some system delay
-                    //Task.Delay(rnd.Next(1, 5));//.Wait();
+                    //Task.Delay(rnd.Next(1, 5)).Wait();
 
                     //Read Data from Vehicle
-                    (string regNr, string owner) = myCar.GetData();
+                    (string regNr, string owner) = localAppConfig.GetData();
 
                     //Verify data consistency - give error if not consistent                   
                     if ((regNr, owner) != ("ABC 123", "Kalle Anka") && (regNr, owner) != ("HKL 556", "Musse Pigg")) 
@@ -54,17 +85,18 @@ namespace ThreadSafeData
 
             var t2 = Task.Run(() =>
             {
+                var localAppConfig = ApplicationConfig.Instance;
                 var rnd = new Random();
                 for (int i = 0; i < 1000; i++)
                 {
                     //Write Data to Vehicle, "HKL 556", "Musse Pigg"
-                    myCar.SetData("HKL 556", "Musse Pigg");
+                    localAppConfig.SetData("HKL 556", "Musse Pigg");
 
                     //introduce some system delay
-                    //Task.Delay(rnd.Next(1, 5));//.Wait();
+                    //Task.Delay(rnd.Next(1, 5)).Wait();
 
                     //Read Data from Vehicle
-                    (string regNr, string owner) = myCar.GetData();
+                    (string regNr, string owner) = localAppConfig.GetData();
 
                     //Verify data consistency - give error if not consistent                   
                     if ((regNr, owner) != ("ABC 123", "Kalle Anka") && (regNr, owner) != ("HKL 556", "Musse Pigg"))
@@ -74,6 +106,7 @@ namespace ThreadSafeData
             });
 
             Task.WaitAll(t1, t2);
+            
             Console.WriteLine("All Finished");
         }
     }
@@ -81,7 +114,7 @@ namespace ThreadSafeData
 /*  Exercise
     1. Make class Vehicle Thread safe using lock(...)
     2.  - Have task t1 write 1000 times "ABC 123", "Kalle Anka" to myCar
-        - Have task t2 write 1000 times "ABC 123", "HKL 556", "Musse Pigg"
+        - Have task t2 write 1000 times "HKL 556", "Musse Pigg" to myCar
         - Verify data consistency
         - Discuss in the group what is data consistency in case of class Vehicle. Is your code living up to it?
 */
